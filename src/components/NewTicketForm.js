daff1073c8ed3e26330689d9fb45d73602094ad4
Kyle Miller <millerkyle85@gmail.com>
2022-08-07 10:14:31 -0700

    create handleNewTicketFormSubmission with props

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,11 +1,41 @@
 import React from "react";
+import { v4 } from 'uuid';
+import PropTypes from "prop-types";
 
 function NewTicketForm(props){
-  return (
-    <React.Fragment>
-      <h3>This is a form.</h3>
-    </React.Fragment>
-  );
-}
+    function handleNewTicketFormSubmission(event) {
+      props.onNewTicketCreation({
+        numberOfStudents: parseInt(event.target.numberOfStudents.value),
+        names: event.target.names.value, 
+        location: event.target.location.value, 
+        issue: event.target.issue.value, 
+        id: v4()
+      });
+    }
+
+
+    return (
+      <React.Fragment>
+        <form onSubmit={handleNewTicketFormSubmission}>
+          <input 
+            type='text'
+            name='names'
+            placeholder='Pair Names' />
+          <input
+            type='text'
+            name='location'
+            placeholder='location' />
+          <textarea 
+            name='issue'
+            placeholder='Describe your issue.' />
+          <button type='submit'>Help!</button>
+        </form>
+      </React.Fragment>
+    );
+  }
+
+NewTicketForm.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
 
 export default NewTicketForm;
